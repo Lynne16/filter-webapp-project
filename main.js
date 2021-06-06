1d13e0cdcea4f3e069544073945f30162013e8c4
Lynne16 <76992610+Lynne16@users.noreply.github.com>
2021-06-06 18:17:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,33 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', getPoses);
+}
+
+Nose_X=0;
+Nose_Y=0;
+
+function getPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+        Nose_X=results[0].pose.nose.x;
+        Nose_Y=results[0].pose.nose.y;
+        console.log("Nose X = " + results[0].pose.nose.x);
+        console.log("Nose Y = " + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded(){
+    console.log('PoseNet Loaded');
 }
 
 function draw(){
+    image(video, 0,0,300,300);
 
 }
 
